facc6dcf2dbbb6753f64de5fe134428a5eb25ba2
ShifatSarwar <57101801+ShifatSarwar@users.noreply.github.com>
2021-09-26 17:29:45 -0500

    Create listen.py

---
+++ functions/listen.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+import programExit
+listener = sr.Recognizer()
+mic= sr.Microphone()
+
+def startListening():
+     
+    with mic as source:
+
+        print("............")
+        listener.adjust_for_ambient_noise(source)
+        audioData = listener.listen(source)
+        
+        response = {
+            "success": True,
+            "error": None,
+            "voicedata": ""
+        }
+
+        try:
+            response["voicedata"] = listener.recognize_google(audioData)
+        except sr.RequestError:
+            response["success"] = False
+            response["error"] = "can't reach google"
+        except sr.UnknownValueError:
+            response["error"] = "couldn't hear you"
+        if(response["success"] == True):
+            print(response["voicedata"])
+            return (response["voicedata"]).lower()
+        else:
+            print(response["error"])
+            programExit.pExit()
+        
