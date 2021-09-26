83107b24e621b0dbeb0087b7b326710c84db9bda
ShifatSarwar <57101801+ShifatSarwar@users.noreply.github.com>
2021-09-26 17:25:27 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 from torch.nn.modules.activation import Softmax
-import listen
-import communicate
+from functions.listen import startListening
+from functions.communicate import setValueID, getValueID, speak
 #import PySimpleGUI as sg
 from PIL import Image, ImageTk, ImageSequence 
-from calculations import addition, subtraction, division, multiplication, tothePower, percentages
-import changeVoice
-from variousSearch import googleSearch, play, findInfo, chooseGenre
+from functions.calculations import addition, subtraction, division, multiplication, tothePower, percentages
+from functions.changeVoice import vChange
+from functions.variousSearch import googleSearch, play, findInfo, chooseGenre
 import datetime
 import random
 import torch
-import programExit
+from functions.programExit import pExit
 import json
-from model import NeuralNet
-from nltkutilities import tokenize, bag_of_words
+from training.model import NeuralNet
+from training.nltkutilities import tokenize, bag_of_words
 #checking import
 
 if __name__ == '__main__':
@@ -26,7 +26,7 @@ if __name__ == '__main__':
        intents = json.load(json_data) 
 
     #File contains model trained
-    FILE = "data.pth"
+    FILE = "training/data.pth"
     data = torch.load(FILE)
 
     input_size = data["input_size"]
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     model.eval()
 
     #loads up chosen voice rather than default voice
-    communicate.setValueID(communicate.getValueID())
+    setValueID(getValueID())
 
   ##  sg.theme('DarkAmber')
   ## gif_filename = r'shapeB.gif'
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     
     #listening starts here
     while True:
-        command = listen.startListening()
+        command = startListening()
      #   for frame in ImageSequence.Iterator(Image.open(gif_filename)):
       #      event, values = window.read(timeout=10)
        #     window['-IMAGE-'].update(data=ImageTk.PhotoImage(frame) )
@@ -67,11 +67,11 @@ if __name__ == '__main__':
             break
 
         elif('change voice' in command):
-            changeVoice.vChange()
+            vChange()
 
         elif('time' in command):
             time = datetime.datetime.now().strftime('%I:%M:%p')
-            communicate.speak("The current time is, " + time)
+            speak("The current time is, " + time)
         else:
             #convert command into a tokenized array
             command = tokenize(command)
@@ -101,7 +101,7 @@ if __name__ == '__main__':
                                 if tokens in command:
                                     command.remove(tokens)
                             videoTitle = ' '.join(command)
-                            communicate.speak(response+videoTitle)
+                            speak(response+videoTitle)
                             play(videoTitle)
 
                         elif tag == 'search':
@@ -110,7 +110,7 @@ if __name__ == '__main__':
                                 if tokens in command:
                                     command.remove(tokens)
                             search = ' '.join(command)
-                            communicate.speak(response+search)
+                            speak(response+search)
                             googleSearch(search)
 
                         elif tag == 'info':
@@ -119,48 +119,48 @@ if __name__ == '__main__':
                                 if tokens in command:
                                     command.remove(tokens)
                             searchInfo = ' '.join(command)
-                            communicate.speak(response)
-                            communicate.speak(findInfo(searchInfo))
+                            speak(response)
+                            speak(findInfo(searchInfo))
 
                         #calculations
                         elif tag == 'add':
                             searchInfo =' '.join(command)
-                            communicate.speak(response+addition(searchInfo))
+                            speak(response+addition(searchInfo))
                         
                         elif tag == 'subtract':
                             searchInfo =' '.join(command)
-                            communicate.speak(response+subtraction(searchInfo))
+                            speak(response+subtraction(searchInfo))
                         
                         elif tag == 'multiply':
                             searchInfo =' '.join(command)
-                            communicate.speak(response+multiplication(searchInfo))
+                            speak(response+multiplication(searchInfo))
                         
                         elif tag == 'divide':
                             searchInfo =' '.join(command)
-                            communicate.speak(response+division(searchInfo))
+                            speak(response+division(searchInfo))
                         
                         elif tag == 'percent':
                             searchInfo =' '.join(command)
-                            communicate.speak(response+percentages(searchInfo))
+                            speak(response+percentages(searchInfo))
                         
                         elif tag == 'power':
                             searchInfo =' '.join(command)
-                            communicate.speak(response+tothePower(searchInfo))
+                            speak(response+tothePower(searchInfo))
                         
                         elif tag == 'suggest':
-                            communicate.speak("Sorry, that is not available yet")
+                            speak("Sorry, that is not available yet")
 
 
                         else:
                             #A response generated by AI
-                            communicate.speak(response)
+                            speak(response)
                     
                                 
             else:
                 #response if user talks jibberish
-                communicate.speak("Sorry, could not understand you")
+                speak("Sorry, could not understand you")
 
-    programExit.pExit()
+    pExit()
   
         
 
